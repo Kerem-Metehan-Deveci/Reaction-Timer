fe7f5d430a8f787ba79b4882f7261c0651f4015d
Kerem Metehan Deveci <keremdeveci100@gmail.com>
2022-08-31 16:08:17 +0300

    Update Click Me Button Color

--- src/components/VBlock.vue
+++ src/components/VBlock.vue
@@ -38,7 +38,8 @@
     .block {
         width: 400px;
         border-radius: 20px;
-        background: #0faf87;
+        background: rgb(34,193,195);
+        background: linear-gradient(0deg, rgba(34,193,195,1) 0%, rgba(253,187,45,1) 100%);
         color: white;
         text-align: center;
         padding: 100px 0;
